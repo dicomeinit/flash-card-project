03fbf1c2a3a8d0d466ef1d06a4bce65e39ce308a
Diana <dicomeinit@gmail.com>
2022-08-26 00:48:05 +0300

    Flipping Cards

--- main.py
+++ main.py
@@ -6,23 +6,38 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black",)
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black",)
+    canvas.itemconfig(card_bg, image=card_font_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_bg, image=card_back_image)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_font_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_font_image)
-card_title = canvas.create_text(400, 150, text="", fill="black", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "bold"))
+card_back_image = PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=card_font_image)
+
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
