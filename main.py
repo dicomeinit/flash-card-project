c32c562872af4fb38c2e20d84a5a8a9926ae6099
Diana <dicomeinit@gmail.com>
2022-08-26 12:50:55 +0300

    saving progress

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import pandas
 from random import *
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -25,6 +32,14 @@ def flip_card():
     canvas.itemconfig(card_bg, image=card_back_image)
 
 
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -41,13 +56,13 @@ card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-right_button_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_image, highlightthickness=0, command=next_card)
-right_button.grid(row=1, column=0)
-
 wrong_button_image = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_button_image, highlightthickness=0, command=next_card)
-wrong_button.grid(row=1, column=1)
+wrong_button.grid(row=1, column=0)
+
+right_button_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_button_image, highlightthickness=0, command=is_known)
+right_button.grid(row=1, column=1)
 
 next_card()
 
